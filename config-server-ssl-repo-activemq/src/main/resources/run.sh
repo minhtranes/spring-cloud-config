276995c6f77caf874f2eef0d37deac8dde12cba2
Minh Tran <wisekingtran@gmail.com>
2018-09-16 10:53:38 +0700

    Enable activemq

--- config-server-ssl-repo-activemq/src/main/resources/run.sh
+++ config-server-ssl-repo-activemq/src/main/resources/run.sh
@@ -1,6 +1,6 @@
 cd $PWD
 java \
     -Dlogging.config=./config/logback.xml \
-    -Djavax.net.ssl.trustStore=./config/trustStore.jks \
-    -Djavax.net.ssl.trustStorePassword=Spsvn@123 \
+    -Djavax.net.ssl.trustStore=./config/cacerts \
+    -Djavax.net.ssl.trustStorePassword=changeit \
     -jar config-server.jar
