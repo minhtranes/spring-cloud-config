82c58a733cb776236bbfe973b4411ee75174f4f5
Minh Tran <wisekingtran@gmail.com>
2018-09-18 11:37:08 +0700

    SSH Repository with embedded ActiveMQ

--- config-server-ssh-repo-embedded-activemq/src/main/java/vn/sps/study/configserver/ConfigServerApplication.java
+++ config-server-ssh-repo-embedded-activemq/src/main/java/vn/sps/study/configserver/ConfigServerApplication.java
@@ -4,9 +4,11 @@ package vn.sps.study.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableJms
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
---
+++ config-server-ssl-repo/src/main/java/vn/sps/study/configserver/config/TestConfiguration.java
@@ -0,0 +1,28 @@
+/*
+ * Class: TestConfiguration
+ *
+ * Created on Sep 18, 2018
+ *
+ * (c) Copyright Swiss Post Solutions Ltd, unpublished work
+ * All use, disclosure, and/or reproduction of this material is prohibited
+ * unless authorized in writing.  All Rights Reserved.
+ * Rights in this program belong to:
+ * Swiss Post Solution.
+ * Floor 4-5-8, ICT Tower, Quang Trung Software City
+ */
+package vn.sps.study.configserver.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestConfiguration {
+
+    @Bean
+    @ConfigurationProperties(prefix="adasd")
+    TestProperties testProperties() {
+        return new TestProperties();
+    }
+}
