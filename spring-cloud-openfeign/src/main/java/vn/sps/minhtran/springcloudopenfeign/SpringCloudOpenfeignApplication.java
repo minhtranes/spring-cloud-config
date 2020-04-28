5e0b4de529f093333ffe5adf53261ec19bb3db83
Minh Tran <thminh_1@spsvietnam.vn>
2020-04-28 19:22:51 +0700

    Initialize spring cloud openfeign

---
+++ spring-cloud-openfeign/src/main/java/vn/sps/minhtran/springcloudopenfeign/SpringCloudOpenfeignApplication.java
@@ -0,0 +1,13 @@
+package vn.sps.minhtran.springcloudopenfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudOpenfeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudOpenfeignApplication.class, args);
+	}
+
+}
